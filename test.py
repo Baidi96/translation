6eea0facb202d1e695c1a001adb919c4207e97ec
Baidi96 <1400012861@pku.edu.cn>
2017-08-25 11:44:44 -0500

    Update test.py

--- test.py
+++ test.py
@@ -2,18 +2,18 @@
 import re
 
 p1 = re.compile("[a-zA-Z]\w*\'?:[a-zA-Z]\w*\'?$") #X;S
-p2 = re.compile("\A\\\\[a][n][d](.+)$") #phi1 /\ phi2
-p3 = re.compile("\A\\\\[n][o][t](.+)$") #not(phi)
-p4 = re.compile("\A\\\\[e][x][i][s][t][s](.+)$") #exists x:s,phi
+p2 = re.compile("\A\\\\[a][n][d]\(.+\)$") #phi1 /\ phi2
+p3 = re.compile("\A\\\\[n][o][t]\(.+\)$") #not(phi)
+p4 = re.compile("\A\\\\[e][x][i][s][t][s]\(.+\)$") #exists x:s,phi
 p5 = re.compile("\A\\\\?[a-zA-Z]+(.+)$") #app(phi1,pih2,...,phin)
 #pattern = str(raw_input("Enter a pattern :"))
 
 def lift(str):
 	if p1.match(str):
 		return "#variable(" + \
-    	       (re.search("([a-zA-Z]\w*\'?):([a-zA-Z]\w*\'?)",line).group(1)) + \
+    	       (re.search("([a-zA-Z]\w*\'?):([a-zA-Z]\w*\'?)",str).group(1)) + \
                ",#sort(" + \
-               (re.search("([a-zA-Z]\w*\'?):([a-zA-Z]\w*\'?)",line).group(2)) + \
+               (re.search("([a-zA-Z]\w*\'?):([a-zA-Z]\w*\'?)",str).group(2)) + \
                 "))"
 	elif p2.match(str):
 		tmp = str[5:len(str)-2] 
@@ -42,24 +42,41 @@ def lift(str):
 			   lift((re.search("([a-zA-Z]\w*\'?):([a-zA-Z]\w*\'?),(.+)",tmp).group(3))) + \
 			   ")"
 	elif p5.match(str):
+		#print str
+		if str.count("(") == 0 :
+			return "(" + str + ") is not pattern"
 		pos_paren = str.index("(")
 		liftList = ""
 		tmp = str[pos_paren+1:len(str)-2]
 		liftList = ""
-		pos = tmp.index(",")
-		i = 0
-		t = """while pos < len(tmp) - 1 :
-			if tmp[i:pos].count('(') == tmp[i:pos].count(')'):
-				liftList += "," + lift(tmp[i:pos]) 
-				i = pos + 1
-			elif tmp[pos+1:len(tmp)].count(",") == 0 : break 
-			else : pos = pos + tmp[pos+1:len(tmp)].index(",") + 1"""
-		return "#application(#symbol(\"" + str[0:pos_paren] + \
-				"\",#sort(...)..." + \
-				liftList + \
-			   ")"
-	else :
-		return "(" + str + ") is not pattern"
+		if tmp.count(",") == 0 :
+			return "#application(#symbol(\"" + str[0:pos_paren] + \
+				   "\",#sort(...),(" + \
+				   lift(str[pos_paren+1:len(str)-1]) + "))"
+		else :
+			pos = tmp.index(",")
+			i = 0
+			while pos < len(tmp) - 1 :
+				if tmp[i:pos].count('(') == tmp[i:pos].count(')'):
+					#print liftList
+					liftList += "," + lift(tmp[i:pos]) 
+					i = pos + 1
+				if pos + 1 >= len(tmp) :
+					break
+				if tmp[pos+1:len(tmp)].count(",") == 0 : 
+					liftList += "," + lift(tmp[i:len(tmp)])
+					break 
+				#print tmp[pos+1:len(tmp)]
+				pos = pos + tmp[i:len(tmp)].index(",") + 1
+			return "#application(#symbol(\"" + str[0:pos_paren] + \
+					"\",#sort(...))" + \
+					liftList + \
+			   	")"
+ 	else :
+ 		if len(str) == 0 :
+ 			return ""
+		else : 
+			return "(" + str + ") is not pattern"
 
 file_object = open('input.txt')
 for line in file_object:
